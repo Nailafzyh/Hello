017ebe487a6f05f56cd046aac3950d87e704967e
naila <nailabinti1@gmil.com>
2025-03-10 03:19:52 +0700

    projek 1

---
+++ src/haiinaii/HaiiNaii.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package haiinaii;
+
+/**
+ *
+ * @author naila
+ */
+public class HaiiNaii {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
